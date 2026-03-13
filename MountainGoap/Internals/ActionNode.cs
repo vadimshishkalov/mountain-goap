2d2d2a52fe20b47270f408a88b99ddac735bc2c6
Vadim Shishkalov <vadimshishkalov@gmail.com>
2026-03-13 15:08:52 +0300

    Remove action node equality comparison (non-working)

--- MountainGoap/Internals/ActionNode.cs
+++ MountainGoap/Internals/ActionNode.cs
@@ -3,7 +3,6 @@
 // </copyright>
 
 namespace MountainGoap {
-    using System.Collections.Generic;
     using Priority_Queue;
 
     /// <summary>
@@ -38,49 +37,6 @@ namespace MountainGoap {
         /// </summary>
         internal HashSet<MountainGoap.Action> AvailableActions { get; } = new();
 
-#pragma warning disable S3875 // "operator==" should not be overloaded on reference types
-        /// <summary>
-        /// Overrides the equality operator on ActionNodes.
-        /// </summary>
-        /// <param name="node1">First node to be compared.</param>
-        /// <param name="node2">Second node to be compared.</param>
-        /// <returns>True if equal, otherwise false.</returns>
-        public static bool operator ==(ActionNode? node1, ActionNode? node2) {
-            if (node1 is null) return node2 is null;
-            if (node2 is null) return node1 is null;
-            if (node1.Action == null || node2.Action == null) return (node1.Action == node2.Action) && node1.StateMatches(node2);
-            return node1.Action.Equals(node2.Action) && node1.StateMatches(node2);
-        }
-#pragma warning restore S3875 // "operator==" should not be overloaded on reference types
-
-        /// <summary>
-        /// Overrides the inequality operator on ActionNodes.
-        /// </summary>
-        /// <param name="node1">First node to be compared.</param>
-        /// <param name="node2">Second node to be compared.</param>
-        /// <returns>True if unequal, otherwise false.</returns>
-        public static bool operator !=(ActionNode? node1, ActionNode? node2) {
-            if (node1 is null) return node2 is not null;
-            if (node2 is null) return node1 is not null;
-            if (node1.Action is not null) return !node1.Action.Equals(node2.Action) || !node1.StateMatches(node2);
-            return node2.Action is null;
-        }
-
-        /// <inheritdoc/>
-        public override bool Equals(object? obj) {
-            if (obj is not ActionNode item) return false;
-            return this == item;
-        }
-
-        /// <inheritdoc/>
-        public override int GetHashCode() {
-            var hashCode = 629302477;
-            if (Action != null) hashCode = (hashCode * -1521134295) + EqualityComparer<ExecutingAction>.Default.GetHashCode(Action);
-            else hashCode *= -1521134295;
-            hashCode = (hashCode * -1521134295) + EqualityComparer<IPlanningStepState>.Default.GetHashCode(State);
-            return hashCode;
-        }
-
         /// <summary>
         /// Cost to traverse this node.
         /// </summary>
@@ -89,26 +45,6 @@ namespace MountainGoap {
         internal float Cost(IReadOnlyState currentState) {
             if (Action == null) return float.MaxValue;
             return Action.GetCost(currentState);
-        }
-
-        private bool StateMatches(ActionNode otherNode) {
-            foreach (var key in State.Keys) {
-                if (!otherNode.State.ContainsKey(key)) return false;
-                var val = State[key];
-                var otherVal = otherNode.State[key];
-                if (val == null && val != otherVal) return false;
-                if (val == null && val == otherVal) continue;
-                if (val is object obj && !obj.Equals(otherVal)) return false;
-            }
-            foreach (var key in otherNode.State.Keys) {
-                if (!State.ContainsKey(key)) return false;
-                var val = otherNode.State[key];
-                var thisVal = State[key];
-                if (val == null && val != thisVal) return false;
-                if (val == null && val == thisVal) continue;
-                if (val is object obj && !obj.Equals(thisVal)) return false;
-            }
-            return true;
         }
     }
 }
